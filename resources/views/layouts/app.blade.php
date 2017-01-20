c6d683251e3d3c466bda9e01c24fb5bf2f0e5d0f
Pierre Bouffier <pierre.bouffier05@gmail.com>
2017-01-20 11:16:46 +0100

    UI Kit.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/uikit.min.css" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/uikit.min.js"></script>
+</head>
+
+<body>
+    <div class="uk-container-expand header">
+        <div uk-grid>
+            <div class="uk-width-expand">
+                 <div class="uk-animation-toggle">
+        <div class="uk-card uk-card-default uk-card-body uk-animation-scale-down">
+            <li class="uk-active uk-animation-scale-down"><a href="">test</a></li>
+        </div>
+    </div>
+    <div class="uk-animation-toggle">
+        <div class="uk-card uk-card-default uk-card-body uk-animation-scale-down">
+            <li class="uk-active uk-animation-scale-down"><a href="">test</a></li>
+        </div>
+    </div>
+    <div class="uk-animation-toggle">
+        <div class="uk-card uk-card-default uk-card-body uk-animation-scale-down">
+            <li class="uk-active uk-animation-scale-down"><a href="">test</a></li>
+        </div>
+    </div>
+            </div>
+            <div class="uk-width-1-5@m uk-width-1-1@s"><img src="/images/logo.svg" alt="Dropzone - Logo"></div>
+        </div>
+    </div>
+</body>
+</html>
