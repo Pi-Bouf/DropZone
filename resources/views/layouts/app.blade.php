4612692477c4003b5242e8970a1e15c1ff92b5c0
Pierre Bouffier <pierre.bouffier05@gmail.com>
2017-01-27 11:57:08 +0100

    Upgrade UI.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,42 +2,43 @@
 <html lang="fr">
 
 <head>
-    <title></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="/css/uikit.min.css" rel="stylesheet">
-    <link href="/css/app.css" rel="stylesheet">
-    <script src="/js/jquery.min.js"></script>
-    <script src="/js/uikit.min.js"></script>
+  <title></title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="/css/uikit.min.css" rel="stylesheet">
+  <link href="/css/app.css" rel="stylesheet">
+  <script src="/js/jquery.min.js"></script>
+  <script src="/js/uikit.min.js"></script>
 </head>
 
 <body>
-    <div class="uk-container-expand header">
-        <div uk-grid>
-            <div class="uk-width-expand">
-                <nav class="uk-navbar-container menu" uk-navbar>
-                    <div class="uk-navbar-left">
-
-                        <ul class="uk-navbar-nav">
-                            <li><a class="parent" href="{{route('home')}}">Accueil</a></li>
-                            <li>
-                                <a class="parent" href="#">Parent</a>
-                                <div class="uk-navbar-dropdown">
-                                    <ul class="uk-nav uk-navbar-dropdown-nav">
-                                        <li><a href="#">Active</a></li>
-                                        <li><a href="#">Item</a></li>
-                                        <li><a href="#">Item</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li><a class="parent" href="#">Item</a></li>
-                        </ul>
-
-                    </div>
-                </nav>
-            </div>
-            <div class="uk-width-1-5@m uk-width-1-1@s"><img src="/images/logo.svg" alt="Dropzone - Logo"></div>
+  <div id="page" class="uk-container-expand">
+    <div class="uk-grid">
+      <div id="slideMenu" class="uk-width-2-10">
+        <ul class="uk-nav uk-nav-parent-icon" data-uk-nav>
+          <li class="parent"><a href="#">Accueil</a></li>
+          <li class="uk-parent parent">
+            <a href="#">Deposer</a>
+            <ul class="uk-nav-sub">
+              <li class="child"><a href="#">Colis</a></li>
+              <li class="child"><a href="#">Transport</a></li>
+            </ul>
+          </li>
+          <li class="parent"><i class="flaticon-search-1"></i><a href="#">Rechercher</a></li>
+          <li class="parent"><a href="#">Se connecter</a></li>
+        </ul>
+      </div>
+      <div id="content" class="uk-width-8-10 right">
+        <div id="header"><img src="/images/Logo.svg" class="logo"></div>
+        <div id="pages">
+          <div id="accueil" class="pages_item">L'accueil !</div>
+          <div id="recher" class="pages_item">Recherche</div>
+          <div id="depot" class="pages_item">zoube</div>
+          <div id="zoube" class="pages_item">sdfsdf</div>
         </div>
+      </div>
     </div>
+  </div>
 </body>
+
 </html>
