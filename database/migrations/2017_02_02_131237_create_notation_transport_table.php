835e392bb001e7ffbe1cb9117bda0345c415bb84
rchauveau <rm.chauveau@gmail.com>
2017-02-02 15:13:38 +0100

    corrections v3 migration

--- database/migrations/2017_02_02_131237_create_notation_transport_table.php
+++ database/migrations/2017_02_02_131237_create_notation_transport_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNotationTransport extends Migration
+class CreateNotationTransportTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2017_02_02_132302_create_question_trans_table.php
+++ database/migrations/2017_02_02_132302_create_question_trans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateQuestionTrans extends Migration
+class CreateQuestionTransTable extends Migration
 {
     /**
      * Run the migrations.
