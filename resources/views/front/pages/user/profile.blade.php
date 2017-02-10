af780a0efe40afa92542bd8e64666016c2af3652
rchauveau <rm.chauveau@gmail.com>
2017-02-10 11:57:41 +0100

    Boom le chicken!

--- resources/views/front/pages/user/profile.blade.php
+++ resources/views/front/pages/user/profile.blade.php
@@ -8,18 +8,27 @@
         <div  class="uk-form">
             <fieldset data-uk-margin>
               @if(Auth::user()->id == $user->id)
-                <h2>Mon profil</h2>
+                <div class="profile_header">Mon profil</div>
               @else
-                <h2>{{$user->firstName}} {{$user->lastName}}</h2>
+                <div class="profile_header">{{$user->firstName}} {{$user->lastName}}</div>
               @endif
 
-              <i class="uk-icon-star"></i>
+
 
               @if($user->picLink==null)
                 <img src="../../images/icon-{{$user->sexe}}.png" class="uk-thumbnail-mini uk-border-circle" alt="">
               @else
                 <img src="../../images/{{$user->picLink}}" class="uk-thumbnail-mini uk-border-circle" alt="">
               @endif
+
+              <div class="">
+                <i class="notation uk-icon-medium uk-icon-star"></i>
+                <i class="notation uk-icon-medium uk-icon-star"></i>
+                <i class="notation uk-icon-medium uk-icon-star"></i>
+                <i class="notation uk-icon-medium uk-icon-star-half-o"></i>
+                <i class="notation uk-icon-medium uk-icon-star-o"></i>
+              </div>
+
               <p>Profil de {{$user->firstName}} {{$user->lastName}}</p>
               <p>{{$user->description}}</p>
 
