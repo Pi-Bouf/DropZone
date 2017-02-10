bef3b6d8befedbdd0e9360fb768c5f004791b495
Pierre Bouffier <pierre.bouffier05@gmail.com>
2017-02-10 11:53:49 +0100

    Zbra

--- resources/views/front/pages/vehicule/edit.blade.php
+++ resources/views/front/pages/vehicule/edit.blade.php
@@ -2,12 +2,17 @@
 
 <div id="profile" class="landing_pages_item">
     <div class="landing_form">
-        <div class="uk-form">
+        <form class="uk-form">
             <fieldset data-uk-margin>
                 <legend style="color: #3498db">{{ $vehicule->marque }} {{ $vehicule->modele }}</legend>
             </fieldset>
-            COUCOU
-        </div>
+            <div class="uk-form-row">
+                <label for="marque">Marque:</label> <input type="text" placeholder="Marque" id="marque" name="marque" value="{{ $vehicule->marque }}">
+            </div>
+            <div class="uk-form-row">
+                <label for="modele">Modele:</label> <input type="text" placeholder="modele" id="modele" name="modele" value="{{ $vehicule->modele }}">
+            </div>
+        </form>
     </div>
 </div>
 
