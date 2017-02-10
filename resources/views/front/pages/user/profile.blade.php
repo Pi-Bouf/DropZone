e6ca0b5eb941d2da418e6551a775acb9c23b76e4
rchauveau <rm.chauveau@gmail.com>
2017-02-10 10:37:51 +0100

    profile

--- resources/views/front/pages/user/profile.blade.php
+++ resources/views/front/pages/user/profile.blade.php
@@ -1,23 +1,29 @@
-@extends('layouts.app', [ 'menu_style' => 'scroll' ]) @section('content')
+@extends('layouts.app', [ 'menu_style' => 'scroll',
+'includesJs' => [],
+'includesCss' => [] ])
+@section('content')
+
 <div id="profile" class="landing_pages_item">
-    <div class="landing_form">
-        <form class="uk-form">
+    <div data-uk-scrollspy="{cls:'uk-animation-fade', delay:300}" class="landing_form">
+        <div  class="uk-form">
             <fieldset data-uk-margin>
-                <legend>Proposer un transport</legend>
-                <div class="uk-form-row"><input type="text" placeholder="qsdqsd"></div>
-                <div class="uk-form-row"><input type="password" placeholder="qsdqsd"></div>
-                <div class="uk-form-row">
+              @if(Auth::user()->id == $user->id)
+                <legend>Mon profil</legend>
+              @else
+                <legend>{{$user->firstName}} {{$user->lastName}}</legend>
+              @endif
+
+              @if($user->picLink==null)
+                <img src="../../images/icon-{{$user->sexe}}.png" class="uk-thumbnail-mini uk-border-circle" alt="">
+              @else
+              <img src="../../images/{{$user->picLink}}" class="uk-thumbnail-mini uk-border-circle" alt="">
+
+              @endif
                   <p>Profil de {{$user->firstName}} {{$user->lastName}}</p>
                   <p>{{$user->description}}</p>
-                    <select>
-						<option>HEY</option>
-						<option>HOY</option>
-					</select>
-                </div>
 
-                <div class="uk-form-row"><button class="uk-button uk-button-primary uk-animation-shake uk-animation-hover">Valider</button></div>
             </fieldset>
-        </form>
+        </div>
     </div>
 </div>
 
