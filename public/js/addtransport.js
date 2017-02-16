5a4f4962aa67b964839a10e2b3d11fb1417f0d19
Jessy Canto <jessy.canto.sio@gmail.com>
2017-02-16 15:34:45 +0100

    ..

--- public/js/addtransport.js
+++ public/js/addtransport.js
@@ -1,18 +1,20 @@
-alert('addtransport.js');
-
 var div;
 var newInput;
 var name=1;
 function addEtape(){
     div = document.getElementById('etape')
     name++;
+    t = document.createTextNode(name+'.'); 
+    div.appendChild(t);
+
     newInput = document.createElement('input');
  
     newInput.setAttribute('type','text');
     newInput.setAttribute('name','villeEtape'+name);
     div.appendChild(newInput);
+    div.appendChild(document.createElement('br'));
     if (name==5){
-        var e = document.getElementById('formAjoutTransport');
+        var e = document.getElementById('divLeft');
         var but = document.getElementById('ajoutButton');
         e.removeChild(but);
     }
@@ -20,13 +22,11 @@ function addEtape(){
 }
 
 function occa(){
-    console.log('occaa');
     document.getElementById('trajetOcca').style.display = "block";
     document.getElementById('trajetRegu').style.display = "none";
 }
 
 function regu(){
-    console.log('regu');
     document.getElementById('trajetOcca').style.display = "none";
     document.getElementById('trajetRegu').style.display = "block";
 }
