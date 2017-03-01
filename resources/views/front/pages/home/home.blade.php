ad5db84fb67bbc45b3da6aa792570ee4cc903ecc
Pierre Bouffier <Pierre Bouffier>
2017-03-01 20:02:14 +0100

    Materialix is coming ! :D <3

--- resources/views/front/pages/home/home.blade.php
+++ resources/views/front/pages/home/home.blade.php
@@ -1,69 +1,113 @@
-@extends('layouts.app', [
-    'menu_style' => 'scroll',
-    'page_title' => 'DropZone - Ajout transport',
-    'includesJs' => ['/js/components/datepicker.min.js', '/js/components/lightbox.min.js', '/js/components/accordion.min.js', '/js/scrollMenu.js'],
-    'includesCss' => ['/css/components/datepicker.min.css', '/css/components/accordion.gradient.min.css']]
-) @section('content')
-<div id="accueil" class="landing_pages_item">
-    COUCOU :D
-</div>
-<div id="expedition" class="landing_pages_item">
-    <div class="landing_form">
-        <form class="uk-form">
-            <fieldset data-uk-margin>
-                <legend>Expedier un colis</legend>
-                <div class="uk-form-row"><input type="text" placeholder="qsdqsd"></div>
-                <div class="uk-form-row"><input type="password" placeholder="qsdqsd"></div>
-                <div class="uk-form-row">
-                    <input type="date" class='datePicker' placeholder="Date ramassage">
-                </div>
-                <div class="uk-form-row">
-                    <select>
-						<option>Oui</option>
-						<option>Non</option>
-					</select>
-                </div>
-                <div class="uk-form-row"><button class="uk-button uk-animation-shake uk-animation-hover">Valider</button></div>
-            </fieldset>
-        </form>
-    </div>
-</div>
-<div id="transport" class="landing_pages_item">
-    <div class="landing_form">
-        <form class="uk-form">
-            <fieldset data-uk-margin>
-                <legend>Proposer un transport</legend>
-                <div class="uk-form-row"><input type="text" placeholder="qsdqsd"></div>
-                <div class="uk-form-row"><input type="password" placeholder="qsdqsd"></div>
-                <div class="uk-form-row">
-                    <select>
-						<option>HEY</option>
-						<option>HOY</option>
-					</select>
-                </div>
+@extends('layouts.app', [ 'menu_style' => 'scroll', 'page_title' => 'DropZone - Ajout transport', 'includesJs' => ['/js/components/datepicker.min.js',
+'/js/components/lightbox.min.js', '/js/components/accordion.min.js', '/js/scrollMenu.js'], 'includesCss' => ['/css/components/datepicker.min.css',
+'/css/components/accordion.gradient.min.css']] ) @section('content')
 
-                <div class="uk-form-row"><button class="uk-button uk-button-primary uk-animation-shake uk-animation-hover">Valider</button></div>
-            </fieldset>
-        </form>
-    </div>
-</div>
-<div id="recherche" class="landing_pages_item">
-    <div class="landing_form">
-        <form class="uk-form">
-            <fieldset data-uk-margin>
-                <legend>Rechercher</legend>
-                <div class="uk-form-row"><input type="text" placeholder="qsdqsd"></div>
-                <div class="uk-form-row"><input type="password" placeholder="qsdqsd"></div>
-                <div class="uk-form-row">
-                    <select>
-						<option>HEY</option>
-						<option>HOY</option>
-					</select>
+<!-- Home Section start -->
+<section id="home" class="scroll-section root-sec grey lighten-5 home-wrap">
+    <div class="sec-overlay">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="home-inner">
+                        <div class="center-align home-content">
+                            <h1 class="home-title">Drop<span>Zone</span></h1>
+                            <h2 class="home-subtitle">Transportez vos colis à moindre prix !</h2>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="uk-form-row"><button class="uk-button uk-button-primary uk-animation-shake uk-animation-hover">Valider</button></div>
-            </fieldset>
-        </form>
+            </div>
+        </div>
+        <!-- .container end -->
+        <div class="section-call-to-area">
+            <div class="container">
+                <div class="row">
+                    <a href="#about" class="btn-floating btn-large button-middle call-to-about section-call-to-btn animated btn-up btn-hidden"
+                        data-section="#about">
+                        <i class="mdi-navigation-expand-more"></i>
+                        </a>
+                </div>
+            </div>
+            <!-- .container end -->
+        </div>
     </div>
-</div>
+</section>
+<!-- #home Section end -->
+
+<!-- About Section start -->
+<section id="about" class="scroll-section root-sec padd-tb-60 grey lighten-5 about-wrap">
+    <div class="container">
+        <div class="row">
+            <div class="clearfix about-inner">
+
+                <div class="col-sm-12 col-md-4">
+                    <div class="person-about">
+                        <h3 class="about-subtitle">My Story</h3>
+                        <p>Hello, I’m a UI/UX Designer &amp; Front End Developer from Victoria, Australia. I hold a master degree
+                            of Web Design from the World University. <br /> And scrambled it to make a type specimen book.
+                            It has survived not only five centuries</p>
+                    </div>
+                </div>
+                <!-- about me description -->
+
+                <div class="col-sm-6 col-md-4">
+                    <div class="person-img wow fadeIn">
+                        <img class="z-depth-1" src="images/person.png" alt="">
+                    </div>
+                </div>
+                <!-- about me image -->
+
+                <div class="col-sm-6 col-md-4">
+                    <div class="person-info">
+                        <h3 class="about-subtitle">Personal Information</h3>
+                        <h5><span>Name :</span> John Doe</h5>
+                        <h5><span>Age :</span> 25 Years</h5>
+                        <h5><span>Phone :</span> +0123456789</h5>
+                        <h5><span>Email :</span> email@domain.com</h5>
+                        <h5><span>Address :</span> Dhaka, Bangladesh</h5>
+                    </div>
+
+                    <div class="about-social">
+                        <ul>
+                            <li>
+                                <a href="#" class="btn-floating waves-effect waves-light white"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-floating waves-effect waves-light white"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-floating waves-effect waves-light white"><i class="fa fa-google-plus"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-floating waves-effect waves-light white"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-floating waves-effect waves-light white"><i class="fa fa-pinterest"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="team" class="scroll-section root-sec brand-bg padd-tb-120 team-wrap">
+    <div class="container">
+        <div class="row">
+
+        </div>
+
+    </div>
+    <div class="section-call-to-area">
+        <div class="container">
+            <div class="row">
+                <a href="#home" class="btn-floating btn-large button-middle call-to-home section-call-to-btn animated btn-up btn-hidden"
+                    data-section="#home">
+                    <i class="mdi-navigation-expand-less"></i>
+                    </a>
+            </div>
+        </div>
+    </div>
+</section>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,47 +1,140 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html class="no-js">
 
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>{{ $page_title }}</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="/css/uikit.min.css" rel="stylesheet">
-	<link href="" rel="stylesheet">
-	<link href="" rel="stylesheet">
-	<link href="/css/app.css" rel="stylesheet">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="images/favicon.png">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="/css/normalize.css">
+	<link rel="stylesheet" href="/font/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="/libs/materialize/css/materialize.min.css" media="screen,projection" />
+	<link rel="stylesheet" href="/css/bootstrap.css" media="screen,projection" />
+
+	<link rel="stylesheet" href="/css/animate.min.css" media="screen,projection" />
+	<link rel="stylesheet" href="/libs/sweetalert/sweet-alert.css" media="screen,projection" />
+
+	<link rel="stylesheet" href="/css/main.css">
+	<link rel="stylesheet" href="/css/responsive.css">
+	<link rel="stylesheet" href="/css/colors/color8.css">
 	@foreach($includesCss as $inc)
 	<link href="{{ $inc }}" rel="stylesheet">
 	@endforeach
-	<script src="/js/jquery.min.js"></script>
-	<script src="/js/uikit.min.js"></script>
-	<script src="/js/global.js"></script>
-	<script src="/js/jquery.slimscroll.min.js"></script>
-	@foreach($includesJs as $inc)
-	<script src="{{ $inc }}"></script>
-	@endforeach
-
 </head>
 
 <body>
-	<div id="page" class="uk-container-expand">
-		<div class="uk-grid">
-			<div id="slideMenu" class="uk-width-2-10">
-				@if($menu_style == "scroll")
-					@include('layouts.menus.menu_scroll')
-				@endif
-				@if($menu_style == "static")
-					@include('layouts.menus.menu_static')
-				@endif
-			</div>
-			<div id="content" class="uk-width-8-10 right">
-				<div id="header"><img src="/images/Logo.svg" class="logo"></div>
-				<div id="pages">
-					@yield('content')
-				</div>
-			</div>
+
+	<!-- Preloader -->
+	<div id="preloader">
+		<div class="loader">
+			<svg class="circle-loader" height="50" width="50">
+				<circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="6" stroke-miterlimit="10" />
+			</svg>
 		</div>
 	</div>
-	@include('front.partials.user_popup')
+	<!--preloader end-->
+
+	<!-- Main Container -->
+	<main id="app" class="main-section">
+		<!-- header navigation start -->
+		<header id="navigation" class="root-sec white nav">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="nav-inner">
+							<nav class="primary-nav">
+								<div class="clearfix nav-wrapper">
+									<a href="#home" class="left brand-logo menu-smooth-scroll" data-section="#home"><img src="images/Logo.svg" alt="">
+									</a>
+									<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+									<ul class="right static-menu">
+										<li>
+											<a class="dropdown-button blog-submenu-init" href="#!" data-activates="dropdown1">
+												<i class="mdi-action-account-circle right"></i>
+											</a>
+										</li>
+									</ul>
+									<ul class="inline-menu side-nav" id="mobile-demo">
+
+										<!-- Mini Profile // only visible in Tab and Mobile -->
+										<li class="mobile-profile">
+											<div class="profile-inner">
+												<div class="pp-container">
+													<img src="images/person.png" alt="">
+												</div>
+												<h3>John Doe</h3>
+												<h5>Creative UI/UX Expert</h5>
+											</div>
+										</li>
+
+
+										<li>
+											<a href="#home" data-section="#home" class="menu-smooth-scroll"><i class="fa fa-user fa-fw"></i>Accueil</a>
+										</li>
+										<li>
+											<a href="#team" data-section="#team" class="menu-smooth-scroll"><i class="fa fa-users fa-fw"></i>Team</a>
+										</li>
+									</ul>
+								</div>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- .container end -->
+		</header>
+		@yield('content')
+		<footer id="footer" class="root-sec white root-sec footer-wrap">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="clearfix footer-inner">
+							<ul class="left social-icons">
+								<li><a href="#" class="tooltips tooltipped facebook" data-position="top" data-delay="50" data-tooltip="Facebook"><i class="fa fa-facebook"></i></a>
+								</li>
+								<li><a href="#" class="tooltips tooltipped linkedin" data-position="top" data-delay="50" data-tooltip="Linkdin"><i class="fa fa-linkedin"></i></a>
+								</li>
+								<li><a href="#" class="tooltips tooltipped twitter" data-position="top" data-delay="50" data-tooltip="Twitter"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li><a href="#" class="tooltips tooltipped google-plus" data-position="top" data-delay="50" data-tooltip="Google Plus"><i class="fa fa-google-plus"></i></a>
+								</li>
+								<li><a href="#" class="tooltips tooltipped dribbble" data-position="top" data-delay="50" data-tooltip="Dribbble"><i class="fa fa-dribbble"></i></a>
+								</li>
+								<li><a href="#" class="tooltips tooltipped behance" data-position="top" data-delay="50" data-tooltip="Behance"><i class="fa fa-behance"></i></a>
+								</li>
+							</ul>
+							<div class="right copyright">
+								<p>Coucou
+									<3</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- #footer end -->
+
+	</main>
+
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="/js/jquery.easing.1.3.js"></script>
+	<script src="/js/detectmobilebrowser.js"></script>
+	<script src="/js/waypoints.js"></script>
+	<script src="/js/jquery.counterup.min.js"></script>
+	<script src="/js/jquery.nicescroll.min.js"></script>
+	<script src="/libs/materialize/js/materialize.min.js"></script>
+	<script src="/libs/sweetalert/sweet-alert.min.js"></script>
+	<script src="/js/common.js"></script>
+	<script src="/js/main.js"></script>
+	@foreach($includesJs as $inc)
+	<script src="{{ $inc }}"></script>
+	@endforeach
 </body>
 
 </html>
