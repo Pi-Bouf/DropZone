3b98b2fca1584e1ef20ad7e2d65dfa9ee3543874
Jessy Canto <jessy.canto.sio@gmail.com>
2017-03-02 16:00:36 +0100

    huhu

--- resources/views/front/pages/expedition/add.blade.php
+++ resources/views/front/pages/expedition/add.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app', [ 
 'menu_style' => 'static', 
 'page_title' => 'DropZone - Ajout expedition',
-'includesJs' => ['/js/addexpedition.js','/js/components/accordion.min.js'],
+'includesJs' => ['/js/addexpedition.js'],
 'includesCss' => ['']])
 @section('content')
 
@@ -10,6 +10,9 @@
         height:400px;
     }
     input{
+        color:black;
+    }
+    textarea{
         color:black;
     }
 </style>
@@ -32,46 +35,66 @@
                             {{ csrf_field() }}
                             <div class="col s12 l5">
                                 <div class="clearfix card-panel grey lighten-5 cform-wrapper">
+                                    <span>Information sur le trajet</span>
                                     <div class="input-field">
                                         <input id="villeDepart" type="text" name="villeDepart" class="validate input-box" onchange="marqueur()">
-                                        <label for="villeDepart" class="input-label">Ville départ :</label>
+                                        <label for="villeDepart" class="input-label">Adresse de départ :</label>
                                         <input id="villeDepartHidden" name="villeDepartHidden" type="hidden" value="">
                                     </div>
                                     <div class="input-field">
                                         <input id="villeArrivee" type="text" name="villeArrivee" class="validate input-box" onchange="marqueur()">
-                                        <label for="villeArrivee" class="input-label">Ville arrivée :</label>
+                                        <label for="villeArrivee" class="input-label">Adresse d'arrivée :</label>
                                         <input id="villeArriveeHidden" name="villeArriveeHidden" type="hidden" value="">
                                     </div>
                                 </div>
                             </div>  
                         </div>
-                        <div class="row">                      
-                            <div class="col s12 l7">
+                        <div class="row">  
+                            <div class="col s0 l2"></div>                    
+                            <div class="col s12 l8">
                                 <div class="clearfix card-panel grey lighten-5 cform-wrapper">
+                                    <span>Information sur le colis</span>
                                     <div class="input-field">
-                                        <input id="villeDepart" type="text" name="villeDepart" class="validate input-box" onchange="marqueur()">
-                                        <label for="villeDepart" class="input-label">Ville départ :</label>
-                                        <input id="villeDepartHidden" name="villeDepartHidden" type="hidden" value="">
+                                        <input id="prix" type="number" name="prix" class="validate input-box" onchange="marqueur()" step="0.01">
+                                        <label for="prix" class="input-label">Prix de la livraison (€) :</label>
                                     </div>
                                     <div class="input-field">
-                                        <input id="villeArrivee" type="text" name="villeArrivee" class="validate input-box" onchange="marqueur()">
-                                        <label for="villeArrivee" class="input-label">Ville arrivée :</label>
-                                        <input id="villeArriveeHidden" name="villeArriveeHidden" type="hidden" value="">
+                                        <textarea id="description" type="text" name="description" class="validate input-box" onchange="marqueur()"></textarea>
+                                        <label for="description" class="input-label">Description :</label>
+                                    </div>
+                                    <div class="input-field">
+                                        <input id="poids" type="number" name="poids" class="validate input-box" onchange="marqueur()">
+                                        <label for="poids" class="input-label">Poids (g) :</label>
+                                    </div>
+                                    <div class="input-field">
+                                        <input id="longueur" type="number" name="longueur" class="validate input-box" onchange="marqueur()">
+                                        <label for="longueur" class="input-label">Longueur (cm) :</label>
+                                    </div>
+                                    <div class="input-field">
+                                        <input id="largeur" type="number" name="largeur" class="validate input-box" onchange="marqueur()">
+                                        <label for="largeur" class="input-label">Largeur (cm) :</label>
+                                    </div>
+                                    <div class="input-field">
+                                        <input id="hauteur" type="number" name="hauteur" class="validate input-box" onchange="marqueur()">
+                                        <label for="hauteur" class="input-label">Hauteur (cm) :</label>
+                                    </div>
+                                        <label>Description :</label>
+                                    <div class="input-field">
+                                        <textarea id="description" type="text" name="description" class="validate input-box" onchange="marqueur()"></textarea>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        </div>
-                        </div>
+                        <input type="submit" id="btProposer" disabled>
                     </form>
                 </div>
-                <div class="section-call-to-area">
-                    <div class="container">
-                        <div class="row">
-                            <a href="#home" class="btn-floating btn-large button-middle call-to-home section-call-to-btn animated btn-up btn-hidden" data-section="#home">
-                            <i class="mdi-navigation-expand-less"></i>
-                            </a>
-                        </div>
+            </div>
+            <div class="section-call-to-area">
+                <div class="container">
+                    <div class="row">
+                        <a href="#home" class="btn-floating btn-large button-middle call-to-home section-call-to-btn animated btn-up btn-hidden" data-section="#home">
+                        <i class="mdi-navigation-expand-less"></i>
+                        </a>
                     </div>
                 </div>
             </div>
