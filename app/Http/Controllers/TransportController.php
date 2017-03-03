8285d63b2d1af5feaed0fbc9c23998ac1b5f304d
Pierre Bouffier <Pierre Bouffier>
2017-03-03 19:35:06 +0100

    Ooops !

--- app/Http/Controllers/TransportController.php
+++ app/Http/Controllers/TransportController.php
@@ -1,15 +1,11 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Ville;
 use App\Etape;
 use Auth;
-
 class TransportController extends Controller
 {
-
     public function __construct()
     {
         $this->middleware('auth');
@@ -19,18 +15,14 @@ class TransportController extends Controller
         $nbVehicule = Auth::user()->vehicules->count();
         return view('front.pages.transport.add', array('nbVehicule' =>$nbVehicule));
     }
-
     public function addData(\App\Transport $transport, Request $request)
     {
-
             
             $transport->vehicule_id = $request->input('vehicule');
             $transport->user_id = Auth::user()->id;
             $transport->detourRetirMax = $request->input('detour');
             $transport->detourDepotMax = $request->input('detour');
             $transport->withHighway = ($request->input('autoroute') == 'oui') ? '1' : '0';
-
-
             if($request->input('nature') == 'occasionnel'){
                 $transport->natureTransport = false;
                 $transport->beginningDate =  $request->input('dateDepart');
@@ -41,8 +33,6 @@ class TransportController extends Controller
                 $transport->regularyBeginningDate = $request->input('dateDebut');
                 $transport->regularyEndingDate = $request->input('dateFin');
             }
-
-
             $transport->information = $request->input('descri');
             $transport->longMax = $request->input('lod');
             $transport->hautMax = $request->input('hd');
@@ -57,15 +47,12 @@ class TransportController extends Controller
                 $villeDep = explode (";", $villeDep);
                 $ville->latitude = $villeDep[0];
                 $ville->longitude = $villeDep[1];
-                $ville->place_id = $villeDep[2];
                 $ville->name = $request->input('villeDepart');
-
                 $villeA = new Ville();
                 $villeArr = $request->input('villeArriveeHidden');
                 $villeArr = explode (";", $villeArr);
                 $villeA->latitude = $villeArr[0];
                 $villeA->longitude = $villeArr[1];
-                $villeA->place_id = $villeArr[2];
                 $villeA->name = $request->input('villeArrivee');
  
                 for($i = 1 ; $i <= 5 ; $i++){
@@ -75,7 +62,6 @@ class TransportController extends Controller
                         $villeEtapeCoord[$i] = explode (";",$villeEtapeCoord[$i]);
                         $villeEtape[$i]->latitude = $villeEtapeCoord[$i][0];
                         $villeEtape[$i]->longitude = $villeEtapeCoord[$i][1];
-                        $villeEtape[$i]->place_id = $villeEtapeCoord[$i][2];
                         $villeEtape[$i]->name = $request->input('villeEtape'.$i);
                         $villeEtape[$i]->save();
                         
@@ -87,22 +73,18 @@ class TransportController extends Controller
                     }
                 }
                 if($ville->save() && $villeA->save()){
-
                     $etapeDepart = new Etape();
                     $etapeDepart->transport_id = $transport->id;
                     $etapeDepart->ville_id = $ville->id;
                     $etapeDepart->ville_position = 1;
                     $etapeDepart->save();
-
                     $etapeArrivee = new Etape();
                     $etapeArrivee->transport_id = $transport->id;
                     $etapeArrivee->ville_id = $villeA->id;
                     $etapeArrivee->ville_position = 7;
                     $etapeArrivee->save();
-
                     return redirect()->route('addtransport')->with('add', 'ok');
                 }
-
             } else {
                 return 'fait chier';
             }
--- database/seeds/VilleTableSeeder.php
+++ database/seeds/VilleTableSeeder.php
@@ -15,14 +15,12 @@ class VilleTableSeeder extends Seeder
             'latitude' => 44.560132,
             'longitude' => 6.080900,
             'name' => "Gap",
-            'place_id' => "ChIJ1ROi2Hg_yxIRYKeX_aUZCAQ",
         ]);
 
         DB::table('villes')->insert([
             'latitude' => 44.560132,
             'longitude' => 6.080900,
             'name' => "Grenoble",
-            'place_id' => "ChIJb76J1ov0ikcRmFOZbs0QjGE",
         ]);
     }
 }
