96caa8034503f93887fc5aa2c6d9cffb7d635caa
rchauveau <rm.chauveau@gmail.com>
2017-03-03 10:42:02 +0100

    search

--- resources/views/front/pages/search/search.blade.php
+++ resources/views/front/pages/search/search.blade.php
@@ -1,51 +1,34 @@
-<!DOCTYPE html>
-<html lang="fr">
+@extends('layouts.app', [ 'menu_style' => 'static', 'page_title' => 'DropZone - Rechercher un colis ou un transport', 'includesJs' => [], 'includesCss' => ['/css/pages/search.css']] ) @section('content')
 
-<head>
-  <title></title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="/css/uikit.min.css" rel="stylesheet">
-  <link href="/css/app.css" rel="stylesheet">
-  <script src="/js/jquery.min.js"></script>
-  <script src="/js/uikit.min.js"></script>
-</head>
-
-<body>
-  <div id="page" class="uk-container-expand">
-    <div class="uk-grid">
-      <div id="slideMenu" class="uk-width-2-10">
-        <ul class="uk-nav uk-nav-parent-icon" data-uk-nav>
-          <li class="parent"><a href="#">Accueil</a></li>
-          <li class="uk-parent parent">
-            <a href="#">Deposer</a>
-            <ul class="uk-nav-sub">
-              <li class="child"><a href="#">Colis</a></li>
-              <li class="child"><a href="#">Transport</a></li>
-            </ul>
-          </li>
-          <li class="parent"><i class="flaticon-search-1"></i><a href="#">Rechercher</a></li>
-          <li class="parent"><a href="#">Se connecter</a></li>
-        </ul>
-      </div>
-      <div id="content" class="uk-width-8-10 right">
-        <div id="header"><img src="/images/Logo.svg" class="logo"></div>
-        <div id="pages">
-          <div id="rechercheInput">
-            <input type="text" id="searchLieuDepart" placeholder="Lieu de départ">
-            <button><=></button>
-            <input type="text" id="searchLieuArrivee" placeholder="Lieu d'arrivée'">
-            <input type="date" id="searchDate" placeholder="date"><br>
-            <input type="submit" value="Rechercher un colis">
-            <input type="submit" value="Rechercher un transport">
-          </div>
-          <div id="searchMap">
-            api
-          </div>
-        </div>
+<section id="about" class="scroll-section root-sec padd-tb-100-30  grey lighten-5">
+  <div class="container">
+    <div class="row">
+      <div class="clearfix ">
+        test
       </div>
     </div>
   </div>
-</body>
+</section>
 
-</html>
+<section id="vehicule" class="scroll-section root-sec brand-bg padd-tb-60 team-wrap">
+  <div class="container">
+      <div class="row">
+        <div class="default-vehicule">
+            <div class="col s12 m12 l6 offset-l3 black-text">
+              <h3 class="about-subtitle white-text center-align">Véhicule</h3>
+              <div class="col s12 m6 l6">
+              </div>
+              <div class="col s12 m6 l6">
+                <div class="person-info marg-top-15px">
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="default-vehicule center-align">
+          <p>Ce membre n'a pas encore ajouté de véhicule...</p>
+        </div>
+      </div>
+  </div>
+</section>
+
+@endsection
