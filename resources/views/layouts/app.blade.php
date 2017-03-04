53a60e0ebe601e4028df381214fe47a6015a9d63
Rchauveau <rm.chauveau@gmail.com>
2017-03-04 20:53:11 +0100

    le menu tout bo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,12 +46,17 @@
         <header class="navbar-fixed">
             <nav>
                 <div class="nav-wrapper navbar-fixed white">
-                    <a href="#" class="brand-logo center"><img style="height: 50px;" src="/images/Logo.svg"></a>
+                    <a href="/" class="brand-logo center"><img style="height: 50px;" src="/images/Logo.svg"></a>
                     <a href="#" data-activates="mobile-nav" class="button-collapse"><i class="mdi mdi-dots-vertical"></i></a>
+                    <ul id="nav-mobile" class="left hide-on-med-and-down" style="margin-right: 25px;">
+                      <li><a href="{{ url('/search') }}"><i class="mdi mdi-magnify left"></i>Rechercher</a></li>
+                      <li><a href="{{ url('/addtransport') }}"><i class="mdi mdi-package-variant-closed left"></i>Transporter un colis</a></li>
+                      <li><a href="{{ url('addcolis') }}"><i class="mdi mdi-cube-send left"></i>Envoyer un colis</a></li>
+                    </ul>
                     <ul id="nav-mobile" class="right hide-on-med-and-down" style="margin-right: 25px;">
-                      <li><a href="#" class="dropdown-button dropdown-hover" data-activates="user-nav"><i class="mdi mdi-account left"></i>Mon Profil</a></li>
-                      <li><a href="{{ url('/user/myvehicules') }}" class="dropdown-button"><i class="mdi mdi-car left"></i>Véhicules</a></li>
-                      <li><a href="{{ url('/logout') }}" class="dropdown-button"><i class="mdi mdi-logout left red-text"></i></a></li>
+                      <li><a href="#" class="dropdown-button dropdown-hover" data-activates="user-nav"><i class="mdi mdi-account left green-text"></i>Mon Profil</a></li>
+                      <li><a href="{{ url('/user/myvehicules') }}"><i class="mdi mdi-car left blue-text"></i>Véhicules</a></li>
+                      <li><a href="{{ url('/logout') }}"><i class="mdi mdi-logout left red-text"></i> </a></li>
                     </ul>
                 </div>
             </nav>
@@ -59,7 +64,13 @@
         <mobile>
             <ul class="side-nav collapsible collapsible-accordion" id="mobile-nav">
               <li><a href="{{ url('/') }}" class="dropdown-button"><i class="mdi mdi-home small left"></i>Accueil</a></li>
-              <li class="bold"><a class="active collapsible-header waves-effect"><i class="mdi mdi-account slamm left"></i>Mon Profil</a>
+              <li><a href="{{ url('/search') }}"><i class="mdi mdi-magnify small left"></i>Rechercher</a></li>
+              <li><a href="{{ url('/addtransport') }}"><i class="mdi mdi-package-variant-closed small left"></i>Transporter un colis</a></li>
+              <li><a href="{{ url('addcolis') }}"><i class="mdi mdi-cube-send small left"></i>Envoyer un colis</a></li>
+              <li class="divider"></li>
+
+
+              <li class="bold"><a class="collapsible-header waves-effect"><i class="mdi mdi-account small green-text left"></i>Mon Profil</a>
                   <div class="collapsible-body no-padding" style="display: none;">
                       <ul>
                           <li><a href="{{ url('/user/me') }}">Afficher</a></li>
@@ -68,7 +79,7 @@
                       </ul>
                   </div>
               </li>
-              <li><a href="{{ url('/user/myvehicules') }}" class="dropdown-button"><i class="mdi mdi-car small left"></i>Véhicules</a></li>
+              <li><a href="{{ url('/user/myvehicules') }}" class="dropdown-button"><i class="mdi mdi-car small blue-text left"></i>Véhicules</a></li>
               <li><a href="{{ url('/logout') }}" class="dropdown-button"><i class="mdi mdi-logout small left red-text"></i>Déconnexion</a></li>
             </ul>
         </mobile>
