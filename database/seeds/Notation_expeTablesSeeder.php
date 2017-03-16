e244aa42729756e94581cddd3fe40acf28a3f357
jcanto05000 <jessy.canto.sio@gmail.com>
2017-03-16 21:23:36 +0100

    seed nota

--- database/seeds/Notation_expeTablesSeeder.php
+++ database/seeds/Notation_expeTablesSeeder.php
@@ -8,21 +8,21 @@ class Notation_expeTablesSeeder extends Seeder{
 			'expeditions_id' => 1,
 			'user_id' => 3,
 			'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,',
-			'note' => 8,
+			'note' => 4,
 			'UserOrTransporter' => 0
 			]);
 		DB::table('notation_expeditions')->insert([
 			'expeditions_id' => 2,
 			'user_id' => 2,
 			'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,',
-			'note' => 6,
+			'note' => 3,
 			'UserOrTransporter' => 1
 			]);
 		DB::table('notation_expeditions')->insert([
 			'expeditions_id' => 3,
 			'user_id' => 1,
 			'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,',
-			'note' => 9,
+			'note' => 5,
 			'UserOrTransporter' => 0
 			]);
 
